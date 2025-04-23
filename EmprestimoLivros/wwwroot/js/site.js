2d4cf9adbf954cfbcfd86ae26bfa83c8a8c82ed6
LucasAuad <lsarsur@hotmail.com>
2025-04-22 22:25:57 -0300

    primeiro commit

---
+++ EmprestimoLivros/wwwroot/js/site.js
@@ -0,0 +1,45 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$(document).ready(function () {
+
+    $('#Emprestimos').DataTable({
+        language:
+        {
+            "decimal": "",
+            "emptyTable": "No data available in table",
+            "info": "Mostrando _START_ a _END_ com um total de _TOTAL_ registros",
+            "infoEmpty": "Showing 0 to 0 of 0 entries",
+            "infoFiltered": "(filtered from _MAX_ total entries)",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": "Mostrar _MENU_ entradas",
+            "loadingRecords": "Loading...",
+            "processing": "",
+            "search": "Procurar:",
+            "zeroRecords": "No matching records found",
+            "paginate": {
+                "first": "Próximo",
+                "last": "Último",
+                "next": "Próximo",
+                "previous": "Anterior"
+            },
+            "aria": {
+                "orderable": "Order by this column",
+                "orderableReverse": "Reverse order this column"
+            }
+        }
+
+
+
+
+    });
+
+    setTimeout(function () {
+        $(".alert").fadeOut("slow", function () {
+            $(this).alert('close');
+        })
+    }, 2000)
+});
